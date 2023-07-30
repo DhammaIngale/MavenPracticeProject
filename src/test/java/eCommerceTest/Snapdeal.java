50459f4d40945a17722a96cdb9c4dd8f9d605b80
arpitdwivedi <arpitdwivedi07@gmail.com>
2023-07-30 23:07:15 +0530

    [arpitdwivedi ]snapdeal testcases added

---
+++ src/test/java/eCommerceTest/Snapdeal.java
@@ -0,0 +1,17 @@
+package eCommerceTest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Snapdeal 
+{
+  @Test
+  public void snapdealTest() throws InterruptedException 
+  {
+	  WebDriver driver = new ChromeDriver();
+	  driver.get("https://www.snapdeal.com/");
+	  Thread.sleep(3000);
+	  driver.close();
+  }
+}
